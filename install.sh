fa107d8195c03164c5620a088bc9d36c0344c073
Ferriit <ferriitalt@gmail.com>
2026-02-19 00:58:32 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,16 +1,31 @@
 #!/bin/env bash
 
-echo "Installing the FMBYAS assembler (fmasm) and emulator (fmemu)"
+# echo "Installing the FMBYAS assembler (fmasm) and emulator (fmemu)"
+# 
+# echo "Compiling fmemu and fmasm"
+# mkdir dist/
+# g++ src/emulator.cpp -o dist/fmemu -lncurses -O3 -march=native -DCURSESSUPPORT -DDISASSEMBLE
+# g++ src/assembler.cpp -o dist/fmasm -O3 -march=native
+# 
+# sudo chmod +x dist/fmemu
+# sudo chmod +x dist/fmasm
+# 
+# sudo mv dist/fmemu /usr/local/bin/fmemu
+# sudo mv dist/fmasm /usr/local/bin/fmasm
+# 
+# echo "Installation completed"
 
-echo "Compiling fmemu and fmasm"
+echo "Temporarily downloading menumake..."
 mkdir dist/
-g++ src/emulator.cpp -o dist/fmemu -lncurses -O3 -march=native -DCURSESSUPPORT -DDISASSEMBLE
-g++ src/assembler.cpp -o dist/fmasm -O3 -march=native
+cd dist
+rm -rf *
+git clone https://github.com/ferriit/menumake-py
 
-sudo chmod +x dist/fmemu
-sudo chmod +x dist/fmasm
+cd ..
+echo "Opening menu..."
 
-sudo mv dist/fmemu /usr/local/bin/fmemu
-sudo mv dist/fmasm /usr/local/bin/fmasm
+python3 dist/menumake-py/src/main.py && python3 dist/menumake-py/src/main.py build
 
-echo "Installation completed"
+echo "Cleaning up..."
+rm -rf dist/
+
